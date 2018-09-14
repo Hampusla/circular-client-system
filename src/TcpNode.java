5f0b08c1efde9248404526ea23ce08393d99786d
ID17jsl <ID17jsl@cs.umu.se>
2018-09-14 16:40:10 +0200

    Changed the creation and connection to Sockets in TcpNode

--- src/TcpNode.java
+++ src/TcpNode.java
@@ -1,10 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.Scanner;
 
 public class TcpNode extends Node{
@@ -13,54 +10,74 @@ public class TcpNode extends Node{
         /*Argument should be in the following format:
          *Java: java {TcpNode,UdpNode} local-port next-host next-port*/
         int localPort, nextPort;
-        String nextHost;
+        /*Check that the number of arguments are correct.*/
         if (argc.length != 3) {
-            //Throw exception?
             System.err.println("Invalid number of arguments! Input should be on the following format: " +
                     "local-port next-host next-port");
             return;
         }
-        /*Create the local port... Check that the arguments are valid*/
+        /*Save the first argument as localPort.
+         *If the argument is not in the correct format print error message and return*/
         try {
             localPort = Integer.parseInt(argc[0]);
         }catch (NumberFormatException e) {
             System.err.println("Argument " + argc[0] + " must be an integer!");
             return;
         }
-        /*Create the port that we are going to connect to... Check that the arguments are valid*/
+        /*Save the third argument as nextPort.
+         *If the argument is not in the correct format print error message and return*/
         try {
             nextPort = Integer.parseInt(argc[2]);
         }catch (NumberFormatException e) {
             System.err.println("Argument " + argc[2] + " must be an integer!");
             return;
         }
-        /*Skapa en adress */
-        nextHost = argc[1];
-        InetSocketAddress nextHostAdress = new InetSocketAddress(nextHost, nextPort);
-
-        /*Check if argument 2 aka argc[1] is on the form of an IP adress or as a name of a host,
-         *Check if it is possible to connect to the adress/host*/
+        /*Create an InetAdress "ipAddress" using argc[1], Then create an InetSocketAddress using the InetAddress*/
+        InetAddress ipAddress;
+        InetSocketAddress nextHostAddress;
+        try {
+            ipAddress = InetAddress.getByName(argc[1]);
+            nextHostAddress = new InetSocketAddress(ipAddress, nextPort);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return;
+        }
+        /*Create the inSocket*/
+        ServerSocket serverSocket;
+        Socket inSocket, outSocket;
+        InputStream inputStream;
+        try {
+            //Lyssnar efter en förfrågan om en koppling till porten localPort....
+            serverSocket = new ServerSocket(localPort);
+            //När en koppling har gjorts skapas en socket som kan kommunicera över den etablerade TCP-kopplingen.
+            //TODO fråga! Måste detta vara i run() på en separat tråd? Eftersom den kommer att vänta på en return från accept()
+            inSocket = serverSocket.accept();
+        } catch (IOException e) {
+            /*TODO fix the error message!*/
+            System.err.println("Fail: Error when making and connecting the inSocket");
+            e.printStackTrace();
+            return;
+        }
+        /*Create the outSocket*/
+        try {
+            /*Create an outSocket*/
+            outSocket = new Socket(ipAddress, nextPort);
+            /*Connect the outSocket to the IP-address and port of the next Node*/
+            outSocket.connect(nextHostAddress);
+        } catch (UnknownHostException e) {
+            System.err.println("Fail: Error when making and connecting the outSocket");
+            e.printStackTrace();
+            return;
+        } catch (IOException e) {
+            System.err.println("Fail: Error when making and connecting the outSocket");
+            e.printStackTrace();
+            return;
+        }
 
         Thread inputThread = new Thread() {
             @Override
             public void run() {
                 /*Create a port that the TCP-node listens to*/
-                ServerSocket serverSocket;
-                Socket inSocket;
-                InputStream inputStream;
-                //Scanner scanner;
-                try {
-                    serverSocket = new ServerSocket(localPort);
-                    inSocket = serverSocket.accept();
-                    inputStream = inSocket.getInputStream();
-                    //scanner = new Scanner(inputStream);
-                } catch (IOException e) {
-                    /*TODO fix the error message!*/
-                    /*TODO add a return?*/
-                    System.err.println("Fail: Could not make a serverSocket...");
-                    e.printStackTrace();
-                    return;
-                }
                 //while sats?
                 //Vänta på ett meddelande och sedan göra något när den får meddelandet.
             }
@@ -69,23 +86,8 @@ public class TcpNode extends Node{
         Thread outputThread = new Thread() {
             @Override
             public void run() {
-                try {
-                    Socket outSocket = new Socket(nextHost, nextPort);
-                    outSocket.connect(nextHostAdress);
-                    /*TODO find out what to do with the outPutStream*/
-                    OutputStream outputStream = new OutputStream() {
-                        @Override
-                        public void write(int b) throws IOException {
-
-                        }
-                    }
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                //While... Decide what we are going to send to the port...
             }
-            //While... Decide what we are going to send to the port...
         };
         outputThread.start();
     }

16151a618e5b85b8145f901812e4d45745f6c95b
Hampus Larsson <id17hln@cs.umu.se>
2018-09-14 11:19:48 +0200

    Added states and shell of processInput function to MessageProtocol

--- src/MessageProtocol.java
+++ src/MessageProtocol.java
@@ -1,2 +1,20 @@
+
 public class MessageProtocol {
+
+    /**
+     * States Nodes can be in.
+     */
+    private static final int ELECTION = 0;
+    private static final int ELECTION_OVER = 1;
+    private static final int MESSAGE = 3;
+
+    public String processInput(String input) {
+
+
+
+        String output = null;
+
+        return output;
+    }
+
 }

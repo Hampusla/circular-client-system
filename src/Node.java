9c1f19d6798231823f8a4596d56fb814bea52ca8
ID17jsl <ID17jsl@cs.umu.se>
2018-09-14 09:58:52 +0200

    Added TODO to TCP Node

--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,4 @@
 public abstract class Node {
-
-
     protected byte[] serialization(String Message) {
         return null;
     }
--- src/TcpNode.java
+++ src/TcpNode.java
@@ -2,14 +2,17 @@ public class TcpNode extends Node{
 
     public static void main(String argc[]) {
 
-        /** Skapa mPort, mAdress, Port*/
+        /*TODO Check that user input is in correct format. */
+        /*Argument should be in the following format:
+         *Java: java {TcpNode,UdpNode} local-port next-host next-port*/
+        /*Skapa mPort, mAdress, Port*/
 
-        /** Skapa socket*/
+        /*Skapa socket*/
 
-        /** Skapa output stream*/
+        /*Skapa output stream*/
 
-        /**Skapa input stream*/
+        /*Skapa input stream*/
 
-        /** Skapa */
+        /*Skapa */
     }
 }

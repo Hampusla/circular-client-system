fa34b5a90b3a8e5d14b41e83c26f3f8daaf1e1a3
Hampus Larsson <id17hln@cs.umu.se>
2018-09-19 17:14:44 +0200

    Update UdpNode with a shell of the node. No functionality implemented.

--- src/UdpNode.java
+++ src/UdpNode.java
@@ -1,7 +1,40 @@
-
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  *
  */
 public class UdpNode extends Node {
+
+    public static void main(String argc[]) throws UnknownHostException{
+
+        //TODO Create port and host for self
+        int port = Integer.parseInt(argc[0]);
+        InetAddress Host = InetAddress.getLocalHost();
+
+        //TODO Create port and  ip for receiver
+        InetAddress nextHost = InetAddress.getByName(argc[1]);
+        int nextPort = Integer.parseInt(argc[2]);
+
+        //TODO Create MessageProtocol
+        String socketID = Host + "," + port;
+        MessageProtocol messageProtocol = new MessageProtocol(socketID);
+
+        //TODO Create Datagram Socket
+        DatagramSocket datagramSocket;
+        try {
+            datagramSocket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            System.out.println("Could not bind to port!");
+            return;
+        }
+
+        //TODO Create loop for setup
+
+        //TODO Create receive and send loop
+
+        //TODO
+    }
 }
